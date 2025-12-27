739876b044873276bff9c7b72814e0ee34e39d8b
DominicDev <s0l0estudi4r@gmail.com>
2025-12-27 10:22:06 +0100

    v0.0.2

---
+++ config.js
@@ -0,0 +1,23 @@
+export const nameBand = 'MOSES SUMNEY';
+export const nameSongs = [
+	"DOOMED", /*1*/
+	"I LIKE IT I LIKE IT", /*2*/
+	"CUT ME", /*3*/
+	"IN BLOOM", /*4*/
+	"VIRILE", /*5*/
+	"CONVEYOR/BOXES", /*6*/
+	"GAGARIN", /*7*/
+	"JILL/JACK", /*8*/
+	"DOOMED V2", /*9*/
+	"BOXES", /*10*/
+	"COLOUOUR", /*11*/
+	"ALSO", /*12*/
+	"NEITHER/NOR", /*13*/
+	"TWO DOGS", /*14*/
+	"BYSTANDERS", /*15*/
+	"ME IN 20 YEARS", /*16*/
+	"KEEPS ME ALIVE", /*17*/
+	"LUCKY ME", /*18*/
+	"AND SO I COME TO ISOLATION", /*19*/
+	"BEFORE YOU GO", /*20*/
+]
